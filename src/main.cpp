5345fe1185eedb97d264cee866f345702097a5ec
koki1019 <lukaodzelashvili@gmail.com>
2023-06-12 13:32:02 +0100

    Made the sprites as a vector

--- src/main.cpp
+++ src/main.cpp
@@ -9,28 +9,25 @@
 
 int main(int argc, char **argv) {
     star_window window("Test", 1280, 720);
-    star_sprite sprite("assets/logo.png", glm::vec2(0, 200), glm::vec2(512, 512));
-    star_sprite sprite2("assets/logo.png", glm::vec2(-15 + 150, 200), glm::vec2(512, 512));
-    star_sprite sprite3("assets/logo.png", glm::vec2(-15 + 150 * 2, 200), glm::vec2(512, 512));
-    star_sprite sprite4("assets/logo.png", glm::vec2(-15 + 150 * 3, 200), glm::vec2(512, 512));
-    star_sprite sprite5("assets/logo.png", glm::vec2(-15 + 150 * 4, 200), glm::vec2(512, 512));
-    star_sprite sprite6("assets/logo.png", glm::vec2(-15 + 150 * 5, 200), glm::vec2(512, 512));
-    star_sprite sprite7("assets/logo.png", glm::vec2(-15 + 150 * 6, 200), glm::vec2(512, 512));
-    star_sprite sprite8("assets/logo.png", glm::vec2(-15 + 150 * 7, 200), glm::vec2(512, 512));
+    std::vector<star_sprite *> sprites = {
+        new star_sprite("assets/logo.png", glm::vec2(0, 200), glm::vec2(512, 512)),
+        new star_sprite("assets/logo.png", glm::vec2(-15 + 150, 200), glm::vec2(512, 512)),
+        new star_sprite("assets/logo.png", glm::vec2(-15 + 150 * 2, 200), glm::vec2(512, 512)),
+        new star_sprite("assets/logo.png", glm::vec2(-15 + 150 * 3, 200), glm::vec2(512, 512)),
+        new star_sprite("assets/logo.png", glm::vec2(-15 + 150 * 4, 200), glm::vec2(512, 512)),
+        new star_sprite("assets/logo.png", glm::vec2(-15 + 150 * 5, 200), glm::vec2(512, 512)),
+        new star_sprite("assets/logo.png", glm::vec2(-15 + 150 * 6, 200), glm::vec2(512, 512)),
+        new star_sprite("assets/logo.png", glm::vec2(-15 + 150 * 7, 200), glm::vec2(512, 512)),
+    };
 
     double previousTime = glfwGetTime();
     int frameCount = 0;
     std::string fps_text;
     window.run([&] {
         window.set_color(25, 25, 100);
-        sprite.show();
-        sprite2.show();
-        sprite3.show();
-        sprite4.show();
-        sprite5.show();
-        sprite6.show();
-        sprite7.show();
-        sprite8.show();
+        for (star_sprite *sprite : sprites) {
+            sprite->show();
+        }
 
         double currentTime = glfwGetTime();
         frameCount++;
@@ -45,6 +42,12 @@ int main(int argc, char **argv) {
             star_text text(fps_text, glm::vec2(5, 0));
         });
     });
+
+    for (star_sprite *sprite : sprites) {
+        delete sprite;
+    }
+
+    sprites.clear();
 
     return 0;
 }
